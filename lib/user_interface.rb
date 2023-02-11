4d4f534ac5093749d805e1848f728c02c273fe22
Yosua Nicolaus <yosuanicolaus@gmail.com>
2023-02-11 19:12:53 +1100

    create display_end and play_again? on UI class
    also swap player and board parameter position
    on the display functions
    because the display_end have "= nil" optional param
    which should be placed at the end

--- lib/user_interface.rb
+++ lib/user_interface.rb
@@ -19,7 +19,7 @@ class UserInterface
     [p1name, p1symb, p2name, p2symb]
   end
 
-  def display_turn(player, board)
+  def display_turn(board, player)
     puts "#{player.name}'s turn <#{player.symbol}>"
     puts board.get_display
   end
@@ -27,5 +27,29 @@ class UserInterface
   def get_move(player)
     puts "@#{player.name} - Enter a number (0 - 8)"
     gets.chomp.to_i
+  end
+
+  def display_end(board, winner = nil)
+    puts '------------------------------'
+    puts board.get_display
+    puts 'Game End!'
+    if winner.nil?
+      puts 'Result - Draw'
+    else
+      puts "Result - #{winner.name} win!"
+    end
+  end
+
+  def play_again?
+    puts 'Would you like to play again? [Y/n]'
+    answer = gets.chomp
+    case answer[0].upcase
+    when 'Y'
+      true
+    when 'N'
+      false
+    else
+      play_again?
+    end
   end
 end

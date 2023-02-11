6830e21e5e400a07012021049934e70d2db52fce
Yosua Nicolaus <yosuanicolaus@gmail.com>
2023-02-11 15:55:14 +1100

    update Player and Board class
    make board return the string display instead of render
    
    make player's property accessible using attr

--- lib/board.rb
+++ lib/board.rb
@@ -8,15 +8,15 @@ class Board
     @board[index] = symbol
   end
 
-  def render
-    puts '   |   |   '
-    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-    puts '___|___|___'
-    puts '   |   |   '
-    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-    puts '___|___|___'
-    puts '   |   |   '
-    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-    puts '   |   |   '
+  def get_display
+    "   |   |   \n" +
+      " #{@board[0]} | #{@board[1]} | #{@board[2]} \n" +
+      "___|___|___\n" +
+      "   |   |   \n" +
+      " #{@board[3]} | #{@board[4]} | #{@board[5]} \n" +
+      "___|___|___\n" +
+      "   |   |   \n" +
+      " #{@board[6]} | #{@board[7]} | #{@board[8]} \n" +
+      '   |   |   '
   end
 end
--- lib/player.rb
+++ lib/player.rb
@@ -1,4 +1,7 @@
 class Player
+  attr_reader :name, :symbol
+  attr_accessor :active
+
   def initialize(name, symbol, active)
     @name = name
     @symbol = symbol
